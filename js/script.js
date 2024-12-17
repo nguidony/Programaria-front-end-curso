e0b4612b82fceaf20033b95a8f564834062c666c
Nicole Guidony <93284934+nguidony@users.noreply.github.com>
2024-12-17 15:05:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+function verificarCampos() {
+    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != "" ) {
+        alert("Prontinho! Você receberá as novidades por email.");
+    } else {
+        alert("Por favor, preencha os campos nome e email!");
+    }
+}
